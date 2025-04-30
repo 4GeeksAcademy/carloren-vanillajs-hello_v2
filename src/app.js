73b6843522db46f01c3b50b8f3358a069c861921
Carlos Lorenzo <carloren.1996@gmail.com>
2025-04-30 10:57:19 +0000

    completado

--- src/app.js
+++ src/app.js
@@ -5,25 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['el gato', 'Perro Sanxe', 'Ayuso', 'Eren Jaeger', 'tu madre'];
+let action = ['se comió', 'robó', 'se cargó', 'prendió fuego a'];
+let what = ['mis deberes', 'mi ordenador', 'mi móvil', 'mi casa', 'mi corazón'];
+let when = ['hace diez siglos', 'mientras dormía', 'hace cero coma', 'mientras veía el betis', 'cuando me fui a japón'];
+
 function excuseGen() {
   //write your code here
-  let who = ['el gato', 'Perro Sanxe', 'Ayuso', 'Eren Jaeger', 'tu madre'];
-  let action = ['se comió', 'robó', 'se cargó', 'prendió fuego a'];
-  let what = ['mis deberes', 'mi ordenador', 'mi móvil', 'mi casa', 'mi corazón'];
-  let when = ['hace diez siglos', 'mientras dormía', 'hace cero coma', 'mientras veía el betis', 'cuando me fui a japón'];
 
   let phrase = [who, action, what, when]
 
   let excuse = "Siento no poder ayudarte, pero ";
 
-  for(let i=0; i<phrase.length; i++){
-    if( i == phrase.length -1){
-      excuse += phrase[i][Math.floor(Math.random()*phrase[i].length)] + '.'
+  for (let i = 0; i < phrase.length; i++) {
+    if (i == phrase.length - 1) {
+      excuse += phrase[i][Math.floor(Math.random() * phrase[i].length)] + '.'
     } else {
-      excuse += phrase[i][Math.floor(Math.random()*phrase[i].length)] + ' ' 
+      excuse += phrase[i][Math.floor(Math.random() * phrase[i].length)] + ' '
     }
   }
-  
+
   document.getElementById("excuse").innerHTML = excuse;
 
   console.log("Hello Rigo from the console!");
